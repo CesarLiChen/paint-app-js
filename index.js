c23be2f06dc9405c754b97fee8b5160425e6b8f7
Cesar Li Chen <cesarli619@gmail.com>
2023-06-06 22:44:32 -0400

    Adds event listening for mouse behaviour

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Linking working");
+const canvas = document.querySelector(".canvas");
+
+let isDrawing = false;
+
+canvas.addEventListener("mousedown", (event) => {
+    console.log("Drawing..");
+    isDrawing = true;
+});
+
+canvas.addEventListener("mouseup", (event) => {
+    console.log("mouse upped");
+    isDrawing = false;
+});
+
+canvas.addEventListener("mouseleave", (event) => {
+    console.log("Stopped drawing")
+    isDrawing = false;
+})
