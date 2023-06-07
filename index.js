bcc9ebbf80188cba2d1782e9b631a0438f2d9960
Cesar Li Chen <cesarli619@gmail.com>
2023-06-07 18:11:41 -0400

    Color changing is working

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const canvas = document.querySelector(".canvas");
 const clearBtn = document.querySelector(".clear");
 const shapes = document.querySelector(".shapes");
+const colorBtn = document.querySelector(".color");
 
 let isDrawing = false;
 let shape = "circleOption"; // Default shape
+let color = "black";
 
 canvas.addEventListener("mousedown", (event) => {
     console.log("Drawing..");
@@ -35,11 +37,19 @@ shapes.addEventListener("click", (event) => {
     if(event.target.className != "shapes") shape = event.target.className;
 });
 
+colorBtn.addEventListener("change", (event) => {
+    console.log(event.target.value);
+    color = event.target.value;
+    for(let i = 0; i < shapes.children.length; i++) {
+        shapes.children[i].style.background = color;
+    }
+});
+
 function paintSpot(x, y) {
     const spot = document.createElement("div");
     spot.className = `spot ${shape}`;
     spot.style.left = `${x-15}px`;
     spot.style.top = `${y-15}px`;
-    spot.style.background = "green";
+    spot.style.background = `${color}`;
     canvas.appendChild(spot);
 }
