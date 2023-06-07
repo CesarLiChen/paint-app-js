3deebddfc5b606aacbe7669b761da1244a9e93ef
Cesar Li Chen <cesarli619@gmail.com>
2023-06-06 23:33:35 -0400

    Drawing with mouse working

--- index.js
+++ index.js
@@ -4,26 +4,29 @@ let isDrawing = false;
 
 canvas.addEventListener("mousedown", (event) => {
     console.log("Drawing..");
+    paintSpot(event.pageX, event.pageY);
     isDrawing = true;
 });
 
-canvas.addEventListener("mouseup", (event) => {
+canvas.addEventListener("mouseup", () => {
     console.log("mouse upped");
     isDrawing = false;
 });
 
-canvas.addEventListener("mouseleave", (event) => {
+canvas.addEventListener("mouseleave", () => {
     console.log("Stopped drawing")
     isDrawing = false;
 });
 
-paintSpot(200, 200);
+canvas.addEventListener("mousemove", (event) => {
+    if (isDrawing) paintSpot(event.pageX, event.pageY);
+});
 
 function paintSpot(x, y) {
     const spot = document.createElement("div");
     spot.className = "spot circleOption";
-    spot.style.left = `${x}px`;
-    spot.style.top = `${y}px`;
+    spot.style.left = `${x-15}px`;
+    spot.style.top = `${y-15}px`;
     spot.style.background = "green";
     canvas.appendChild(spot);
 }
